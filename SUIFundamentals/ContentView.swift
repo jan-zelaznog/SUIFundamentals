3b060ac364c0acfda3662bbef8e8999c73588f38
jan-zelaznog <jan.zelaznog@gmail.com>
2024-10-19 12:26:30 -0600

    Initial Commit

---
+++ SUIFundamentals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUIFundamentals
+//
+//  Created by Ángel González on 19/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUIFundamentals/SUIFundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIFundamentalsApp.swift
+//  SUIFundamentals
+//
+//  Created by Ángel González on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SUIFundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
